778b0890f3e3216544c9b1f4628e23e4cb095d12
josembar <jose.barrantes@hotmail.es>
2024-03-19 00:07:34 -0500

    feat: instrument app

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
 @app.get("/hello")
 def read_root():
     return {"messsage": "Hello!"}
+
+FastAPIInstrumentor.instrument_app(app)
