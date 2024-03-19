fa9cc8a7e0d6792269cb6938e96ecd97b41bd999
josembar <jose.barrantes@hotmail.es>
2024-03-19 00:19:46 -0500

    feat: manual instrumentation of metrics

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader, ConsoleMetricExporter
+
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
+
+# Service name is required for most backends
+resource = Resource(attributes={
+    SERVICE_NAME: "test-service"
+})
+
+# reader = PeriodicExportingMetricReader(OTLPMetricExporter())
+reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meterProvider)
 
 @app.get("/hello")
 def read_root():
