8c286ed9ac290f2bb9b660b21f4415832ac222c4
josembar <jose.barrantes@hotmail.es>
2024-03-20 20:39:24 -0500

    feat: added otel image

--- aws-resources/build-image.sh
+++ aws-resources/build-image.sh
@@ -3,12 +3,19 @@
 AWS_REGION="us-east-1"
 AWS_ACCOUNT_ID="989307503852"
 IMAGE_NAME="test-app"
+OTEL_IMAGE_NAME="otel"
 REPO_NAME="test"
 
 docker build -t $IMAGE_NAME ../app
+
+docker build -t $OTEL_IMAGE_NAME ./otel
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
 
+docker tag $OTEL_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$OTEL_IMAGE_NAME-latest
+
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$OTEL_IMAGE_NAME-latest
