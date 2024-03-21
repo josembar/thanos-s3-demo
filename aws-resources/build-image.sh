568783d769d1aa1b15b0df41d446a0f0aaa0bba3
josembar <jose.barrantes@hotmail.es>
2024-03-21 09:03:11 -0500

    feat: build thanos image

--- aws-resources/build-image.sh
+++ aws-resources/build-image.sh
@@ -4,11 +4,14 @@ AWS_REGION="us-east-1"
 AWS_ACCOUNT_ID="989307503852"
 IMAGE_NAME="test-app"
 OTEL_IMAGE_NAME="otel"
+THANOS_IMAGE_NAME="thanos-sidecar"
 REPO_NAME="test"
 
 docker build -t $IMAGE_NAME ../app
 
 docker build -t $OTEL_IMAGE_NAME ./otel
+
+docker build -t $THANOS_IMAGE_NAME ./thanos
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
@@ -16,6 +19,10 @@ docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/
 
 docker tag $OTEL_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$OTEL_IMAGE_NAME-latest
 
+docker tag $THANOS_IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$THANOS_IMAGE_NAME-latest
+
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME
 
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$OTEL_IMAGE_NAME-latest
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$THANOS_IMAGE_NAME-latest
